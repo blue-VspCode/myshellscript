ed4c16374c58e699f403ee79ebadeb56afd54670
blue_VspCode <saipallavivanaparthy47@gmail.com>
2024-04-18 21:19:32 +0530

    hello new change

---
+++ 02-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring of varible
+PERSON1=RANI
+PERSON2=RAJU
+
+#reffering variable
+echo "$PERSON1:hello $PERSON2"
+echo "$PERSON2: hi $PERSON1"
+echo "$PERSON1:come to court "
+echo "$PERSON2: yes coming"
