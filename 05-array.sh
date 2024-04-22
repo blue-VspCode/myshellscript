4a7b7308703a85935c6d5a98daf4fc534272fa36
blue_VspCode <saipallavivanaparthy47@gmail.com>
2024-04-22 19:20:41 +0530

    hello folder

---
+++ 05-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MOVIES=("Mango" "Apple" "Banana")
+
+#size of array is 3.
+#index are 0,1,2
+#array always starts with 0.
+echo " First Fruit is :${FRUITS[0]}"
+echo " Second Fruit is :${FRUITS[1]}"
+echo " Third Fruit is :${FRUITS[2]}"
