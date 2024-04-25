71935d5d29426329fd380362bb96deabfb17871e
blue_VspCode <saipallavivanaparthy47@gmail.com>
2024-04-25 15:59:30 +0530

    new file

--- 10-installation.sh
+++ 10-installation.sh
@@ -5,10 +5,31 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
    echo "Please run this script with root acess."
+   exit 1 # manually exit if error occurs.
 else
    echo " you're super user."
 fi
 
 dnf install mysql -y
 
-echo "is script procedding?"
+if [$? -ne 0]
+then
+  echo "Installation of mysql..Failure"
+  exit 1
+else
+  echo " Installation of mysql is Success"
+fi
+
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of git...FAILURE"
+    exit 1
+else
+    echo "Installation of Git...SUCCESS"
+fi
+
+
+echo "is script proceeding?"
+
